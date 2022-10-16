f629c2caa4a8f2e10ea44edb86d113300754695c
NB_Yarik_from_Tagan <y.krivoff@gmail.com>
2022-10-16 13:51:39 +0300

    Add Task1

--- Task1.py
+++ Task1.py
@@ -3,5 +3,22 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+from random import randint
+import os
+os.system('cls')
 
+n = int(input("Введите размерность списка N = "))
+min = int(input("Введите минимальное значение списка: "))
+max = int(input("Введите максимальное значение списка: "))
 
+lst = [randint(min, max) for i in range(n)]
+print(lst)
+
+lst_new = []
+i = 1
+while i < len(lst):
+    lst_new.append(lst[i])
+    i +=2
+print(lst_new)
+
+print(sum(lst_new))
