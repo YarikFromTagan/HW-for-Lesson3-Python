a4bfcafaf4aa02e934f2c46ac759130017d32891
NB_Yarik_from_Tagan <y.krivoff@gmail.com>
2022-10-16 19:57:58 +0300

    Add Task2

--- Task2.py
+++ Task2.py
@@ -4,6 +4,17 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+import os
+import math
+from random import randint
+os.system("cls")
 
+n = int(input("Введите размерность списка N = "))
+min = int(input("Введите минимальное значение списка: "))
+max = int(input("Введите максимальное значение списка: "))
 
+lst = [randint(min, max) for i in range(n)]
 
+lst_new = [lst[i]*lst[-i-1] for i in range(math.ceil(n/2))]
+
+print(f'{lst} => {lst_new}')
